7b6cc586c0558ae64d4930b578dcb1df44c98c21
lmedhub <lucas_hubner1@hotmail.com>
2024-01-04 14:54:41 -0300

    basic crud

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
+import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const AnyComponent = Component as any;
   return (
-    <Component {...pageProps} />
+    <SessionProvider session={pageProps.session}>
+      <AnyComponent {...pageProps} />
+    </SessionProvider>
   );
 };
 
